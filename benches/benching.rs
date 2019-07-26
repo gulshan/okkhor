13f53595c4c5d713a46bdad04ca1cd701de57eab
Gulshanur Rahman <gulshanur@gmail.com>
2019-07-26 16:23:23 +0600

    minor changes

--- benches/benching.rs
+++ benches/benching.rs
@@ -10,38 +10,38 @@ use rupantor::avro::AvroPhonetic;
 
 fn parse_benchmark(c: &mut Criterion) {
     c.bench_function("okkhor ami", |b| b.iter(|| parse(black_box("ami"))));
-    c.bench_function("rupantor ami", move |b| {
+    c.bench_function("rupantor ami", |b| {
         let avro = AvroPhonetic::new();
         b.iter(|| avro.convert(black_box("ami")))
     });
 
     c.bench_function("okkhor kormo", |b| b.iter(|| parse(black_box("korrm"))));
-    let avro = AvroPhonetic::new();
-    c.bench_function("rupantor kormo", move |b| {
+    c.bench_function("rupantor kormo", |b| {
+        let avro = AvroPhonetic::new();
         b.iter(|| avro.convert(black_box("korrm")))
     });
 
     c.bench_function("okkhor bistarito", |b| {
         b.iter(|| parse(black_box("bistarito")))
     });
-    let avro = AvroPhonetic::new();
-    c.bench_function("rupantor bistarito", move |b| {
+    c.bench_function("rupantor bistarito", |b| {
+        let avro = AvroPhonetic::new();
         b.iter(|| avro.convert(black_box("bistarito")))
     });
 
     c.bench_function("okkhor long word", |b| {
         b.iter(|| parse(black_box("ghoTOt``kc")))
     });
-    let avro = AvroPhonetic::new();
-    c.bench_function("rupantor long word", move |b| {
+    c.bench_function("rupantor long word", |b| {
+        let avro = AvroPhonetic::new();
         b.iter(|| avro.convert(black_box("ghoTOt``kc")))
     });
 
     c.bench_function("okkhor sentence 1", |b| {
         b.iter(|| parse(black_box("ami banglay gan gai")))
     });
-    let avro = AvroPhonetic::new();
-    c.bench_function("rupantor sentence 1", move |b| {
+    c.bench_function("rupantor sentence 1", |b| {
+        let avro = AvroPhonetic::new();
         b.iter(|| avro.convert(black_box("ami banglay gan gai")))
     });
 
@@ -52,8 +52,8 @@ fn parse_benchmark(c: &mut Criterion) {
             ))
         })
     });
-    let avro = AvroPhonetic::new();
-    c.bench_function("rupantor sentence 2", move |b| {
+    c.bench_function("rupantor sentence 2", |b| {
+        let avro = AvroPhonetic::new();
         b.iter(|| {
             avro.convert(black_box(
                 "amader valObasa hoye gel ghas, kheye gel goru ar diye gelo ba^sh",
