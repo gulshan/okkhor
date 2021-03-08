16a268bc645c4a3599bdd3fea782a3e79efa1bbd
Gulshanur Rahman <gulshanur@gmail.com>
2021-03-08 21:14:26 +0600

    Version up and visibility update

--- src/models.rs
+++ src/models.rs
@@ -1,35 +1,33 @@
-pub enum MatchType {
+pub(crate) enum MatchType {
     Vowel,
     Consonant,
     Punctuation,
     Char(char),
 }
 
-pub enum Match {
+pub(crate) enum Match {
     PrefixIs(MatchType),
     SuffixIs(MatchType),
     PrefixIsNot(MatchType),
     SuffixIsNot(MatchType),
 }
 
-pub struct Rule {
+pub(crate) struct Rule {
     pub when_matches: &'static [Match],
     pub replace_with: &'static str,
 }
 
-pub struct Pattern {
+pub(crate) struct Pattern {
     pub find: &'static str,
     pub rules: &'static [Rule],
     pub default_replacement: &'static str,
 }
 
-const NO_RULES: &[Rule] = &[];
-
 impl Pattern {
     pub const fn simple_replace(find: &'static str, replace: &'static str) -> Pattern {
         Pattern {
             find: find,
-            rules: NO_RULES,
+            rules: &[],
             default_replacement: replace,
         }
     }
