c1bc0b2fcdaea064e2e45d505770e2ec753a76aa
Gulshan <gulshanur@gmail.com>
2021-05-29 21:45:36 +0600

    Remove inline attribute

--- src/parser.rs
+++ src/parser.rs
@@ -93,7 +93,6 @@ impl Parser {
         }
     }
 
-    #[inline]
     pub(crate) fn find_pattern(&self, input: &str) -> Option<&Pattern> {
         self.patterns
             .range(..=input)
@@ -104,7 +103,6 @@ impl Parser {
 }
 
 impl Pattern {
-    #[inline]
     pub(crate) fn get_replacement(&self, input: &str, prefix: char) -> &str {
         if self.rules.is_empty() {
             self.default_replacement
